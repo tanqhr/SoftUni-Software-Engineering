5b9f440b403632e16efafd2ec6397f64e6c4e312
tanqhr <tanqhr@abv.bg>
2023-01-22 17:15:34 +0200

    SoftUni

---
+++ Java-OOP/scr/WorkingWithAbstraction/Student/Main.java
@@ -0,0 +1,18 @@
+package Student;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StudentSystem studentSystem = new StudentSystem();
+        String[] input = scanner.nextLine().split(" ");
+        while (!input[0].equals("Exit")){
+
+            studentSystem.executeCommand(input);
+           input=scanner.nextLine().split(" ");
+            }
+
+        }
+    }
+
