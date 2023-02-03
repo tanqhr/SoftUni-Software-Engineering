3f94e6e5f0ae3f2371059410567c7accff658dea
tanqhr <tanqhr@abv.bg>
2023-02-03 17:47:08 +0200

    new exercise

---
+++ Java-OOP/scr/Encapsulation/SalaryIncrease_02/Person.java
@@ -0,0 +1,55 @@
+package SalaryIncrease_02;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+
+
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+     public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double percentage){
+        if(this.age>30){
+            this.salary=salary+salary*percentage/100;
+        }else{
+            this.salary=salary+salary*percentage/200;
+        }
+
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("%s %s gets %f", this.getFirstName(), this.getLastName(), this.salary);
+    }
+}
+
+
