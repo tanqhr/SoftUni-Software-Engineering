35675e238d283186bea41729873d0267a090ea78
tanqhr <tanqhr@abv.bg>
2023-02-07 19:33:02 +0200

    add exercise

---
+++ Java-OOP/scr/EncapsulationExercise/ClassBox_01/Box.java
@@ -0,0 +1,49 @@
+package ClassBox_01;
+
+public class Box {
+    private double length;
+    private double width;
+    private double height;
+
+
+    public Box(double length, double width, double height) {
+        this.setLength(length);
+        this.setWidth(width);
+        this.setHeight(height);
+    }
+
+    private void setLength(double length){
+        if(length>0){
+            this.length=length;
+        }else{
+            throw new IllegalArgumentException("Length cannot be zero or negative.");
+        }
+
+    }
+    private void setWidth(double width){
+        if(width > 0){
+            this.width = width;
+        }else{
+            throw new IllegalArgumentException("Width cannot be zero or negative.");
+        }
+
+    }
+    private void setHeight(double height){
+        if(height > 0){
+            this.height = height;
+        }else{
+            throw new IllegalArgumentException("Height cannot be zero or negative.");
+        }
+
+    }
+    public double calculateSurfaceArea(){
+        return  (2 * this.length * this.width) + (2 * this.length * this.height) + (2 * this.width * this.height);
+    }
+    public double calculateLateralSurfaceArea (){
+        return 2 * this.length * this.height + 2 * this.width * this.height;
+    }
+    public double calculateVolume (){
+        return  this.length * this.width *this.height;
+    }
+
+}
