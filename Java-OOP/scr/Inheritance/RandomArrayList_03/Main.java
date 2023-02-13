15471e09624a0f8e9ccc3b55a4f8f26066d345af
tanqhr <tanqhr@abv.bg>
2023-02-13 12:56:04 +0200

    write new task in OOP

---
+++ Java-OOP/scr/Inheritance/RandomArrayList_03/Main.java
@@ -0,0 +1,8 @@
+package RandomArrayList_03;
+
+public class Main {
+    public static void main(String[] args) {
+        RandomArrayList randomArrayList = new RandomArrayList();
+        randomArrayList.getRandomElement();
+    }
+}
---
+++ Java-OOP/scr/Inheritance/RandomArrayList_03/RandomArrayList.java
@@ -0,0 +1,11 @@
+package RandomArrayList_03;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomArrayList extends  ArrayList {
+    public Object getRandomElement(){
+        int index = new Random().nextInt(super.size());
+        return super.remove(index);
+    }
+}
