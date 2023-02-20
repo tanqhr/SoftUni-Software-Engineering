25fec6ad875784c663fcaaa28bad351cc7cc7141
tanqhr <tanqhr@abv.bg>
2023-02-20 17:17:55 +0200

    Adding task and folder

---
+++ Java-OOP/scr/InterfacesAndAbstraction/CarShop_01/Car.java
@@ -0,0 +1,15 @@
+package CarShop_01;
+
+public interface Car {
+
+    int TIRES=4;
+
+    String getModel();
+
+    String getColor();
+
+    Integer getHorsePower();
+
+    String countryProduced();
+
+}
---
+++ Java-OOP/scr/InterfacesAndAbstraction/CarShop_01/Main.java
@@ -0,0 +1,13 @@
+package CarShop_01;
+
+public class Main {
+    public static void main(String[] args) {
+
+       Car seat = new Seat("Leon", "gray", 110, "Spain");
+
+        System.out.printf("%s is %s color and have %d horse power %n", seat.getModel(), seat.getColor(), seat.getHorsePower());
+
+        System.out.println(seat.toString());
+
+    }
+}
