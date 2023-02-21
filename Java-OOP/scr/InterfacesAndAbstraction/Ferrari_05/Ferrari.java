61aead6babfa30c025128125738c2a3091ae96c9
tanqhr <tanqhr@abv.bg>
2023-02-21 23:27:16 +0200

    task Ferrari

---
+++ Java-OOP/scr/InterfacesAndAbstraction/Ferrari_05/Ferrari.java
@@ -0,0 +1,26 @@
+package Ferrari;
+
+public class Ferrari implements Car{
+
+    private String driverName;
+    private static final String MODEL="488-Spider";
+
+    public Ferrari(String driverName) {
+        this.driverName = driverName;
+    }
+
+    @Override
+    public String brakes() {
+        return "Brakes!";
+    }
+
+    @Override
+    public String gas() {
+        return "brum-brum-brum-brrrrr";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s/%s/%s/%s",MODEL,brakes(),gas(),this.driverName);
+    }
+}
