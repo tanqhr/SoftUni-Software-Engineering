3ef121eb30650c598b132fdb78cd6d82de4229c0
tanqhr <tanqhr@abv.bg>
2023-03-02 15:16:29 +0200

    task Animals

---
+++ Java-OOP/scr/Polymorphism/Animals_03/Animal.java
@@ -0,0 +1,33 @@
+package Animals;
+
+public abstract class Animal {
+    private String name;
+    private String favouriteFood;
+
+    public Animal(String name, String favouriteFood) {
+        this.setName(name);
+        this.setFavouriteFood(favouriteFood);
+    }
+
+
+    public String explainSelf() {
+        return String.format("I am %s and my favourite food is %s", this.getName(), this.getFavouriteFood());
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFavouriteFood() {
+        return favouriteFood;
+    }
+
+    public void setFavouriteFood(String favouriteFood) {
+        this.favouriteFood = favouriteFood;
+    }
+}
