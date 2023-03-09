aff4113f5547d7dee7c3008d89ae275ed649cd51
tanqhr <tanqhr@abv.bg>
2023-03-10 01:15:53 +0200

    task

---
+++ Java-OOP/scr/PolymorphismExercises/VehiclesExtend_02/Car.java
@@ -0,0 +1,12 @@
+package VehiclesExtend_02;
+
+public class Car extends Vehicle {
+
+    public final static double AC_ADDITIONAL_CONSUMPTION = 0.9;
+
+    public Car(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        super(fuelQuantity, fuelConsumption, tankCapacity, AC_ADDITIONAL_CONSUMPTION);
+    }
+
+}
+
---
+++ Java-OOP/scr/PolymorphismExercises/VehiclesExtend_02/Truck.java
@@ -0,0 +1,18 @@
+package VehiclesExtend_02;
+public class Truck extends Vehicle {
+
+    public final static double AC_ADDITIONAL_CONSUMPTION = 1.6;
+    public final static double FUEL_AFTER_DRIVER_DEDUCTION = 0.95;
+
+    public Truck(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        super(fuelQuantity, fuelConsumption, tankCapacity, AC_ADDITIONAL_CONSUMPTION);
+    }
+
+
+
+    @Override
+    public void refuel(double liters) {
+        super.refuel(liters * FUEL_AFTER_DRIVER_DEDUCTION);
+    }
+
+}
